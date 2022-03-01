1f98d295d5f2c1dcfc7f15f5ac86d3b9eaf23b05
Sittipong19 <75230330+Sittipong19@users.noreply.github.com>
2022-03-01 12:22:11 +0700

    Add files via upload

---
+++ ebd-642-w11-Makly30-main/Components/Footatge.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="py-12 bg-yellow-100">
+        <h1 class="text-center">Made By Makly Sim 6214421030</h1>
+    </div>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
